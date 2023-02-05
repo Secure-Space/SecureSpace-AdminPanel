66d943c7db783064ff13ad36c8abc95ffd3a1c2e
chetanmanoj <chetanmanoj2001@gmail.com>
2023-02-05 15:28:19 +0530

    Created routes for all pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 import './App.css';
-import DashBoard from './components/DashBoard';
 
 import "@fontsource/ubuntu";
+
+import Home from "./pages/home/Home";
+import Feed from "./pages/feed/Feed";
+import Notification from "./pages/notification/Notification";
+import Profile from "./pages/profile/Profile";
 
 
 function App() {
   return (
     <div className='App'>
-     <DashBoard/>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/">
+            <Route index element = {<Home/>}/>
+            <Route path = "/feed" element={<Feed/>}/>
+            <Route path = "/notif" element={<Notification/>}/>
+            <Route path = "/profile" element={<Profile/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
