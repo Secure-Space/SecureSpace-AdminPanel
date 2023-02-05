20f968fc7699830b3f71509ba289a3aa75cddafc
chetanmanoj <chetanmanoj2001@gmail.com>
2023-02-05 17:42:00 +0530

    Added link routes to sidebar elements

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 import "./SideBar.scss";
 
@@ -20,22 +21,30 @@ const SideBar = () => {
       </div>
       <div className="contents">
         <ul>
+        <Link to="/" className="link">
           <li>
             <UilEstate />
             <span className="text">Home</span>
           </li>
+          </Link>
+          <Link to="/feed" className="link">
           <li>
             <UilVideo />
             <span className="text">Feed</span>
           </li>
+          </Link>
+          <Link to="/noti" className="link">
           <li>
             <UilBell />
             <span className="text">Notification</span>
           </li>
+          </Link>
+          <Link to="/profile" className="link">
           <li>
             <UilUser />
             <span className="text">Profile</span>
           </li>
+          </Link>
           <li>
             <UilSignout />
             <span className="text">Sign Out</span>
