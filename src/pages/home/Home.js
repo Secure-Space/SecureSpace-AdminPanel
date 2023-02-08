109f215f1d8b6ada56b7f3f4698efe0ee9f46c4f
chetanmanoj <chetanmanoj2001@gmail.com>
2023-02-08 19:07:04 +0530

    Added content to home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import { useState } from "react";
+import Calendar from "react-calendar";
 
-import './Home.scss';
+import "./Home.scss";
+import "./Calendar.css";
 
-import SideBar from '../../components/SideBar/SideBar';
+import SideBar from "../../components/SideBar/SideBar";
 
 const Home = () => {
-  return (
-    <div className='Home'>
-        <SideBar/>
-        <div className='container'>
-            <h1>Home</h1>
-        </div>
-    </div>
-  )
-}
+  const [date, setDate] = useState(new Date());
 
-export default Home
+  return (
+    <div className="Home">
+      <SideBar />
+      <div className="container">
+        <h1 className="title">Home</h1>
+        <div className="contentcontainer">
+        <div className="row1">
+          <div className="Calcontainer">
+            <div className="subtitle">
+              <text>Calendar</text>
+            </div>
+            <Calendar onChange={setDate} value={date} />
+          </div>
+         <div className="Surveicontainer">
+         <div className="subtitle">
+              <text>Surveillance</text>
+            </div>
+         </div>
+        </div>
+        <div className="row2">
+        <div className="subtitle">
+              <text>User Activities</text>
+            </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Home;
