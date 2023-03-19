9cc52891aa5f74e6a8f8595ad361860ba60299a5
chetanmanoj <chetanmanoj2001@gmail.com>
2023-03-19 21:20:59 +0530

    Cleaned up code

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-// const helmet = require('helmet');
+
 
 require('dotenv').config();
 
