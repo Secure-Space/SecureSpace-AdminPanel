89958a7a9decb8c180ab623800b0e4d7d9cab66b
chetanmanoj <chetanmanoj2001@gmail.com>
2023-03-28 01:47:46 +0530

    Added the profile API to Home page

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,18 +1,43 @@
-// import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./Home.scss";
 
 import SideBar from "../../components/SideBar/SideBar";
 
 const Home = () => {
+  const [uname, setUname] = useState('')
+  async function displayProfile(){
+    const response  = await fetch('http://localhost:8000/api/profile',{
+    headers: {
+      'x-access-token':localStorage.getItem('token'),
+    }
+    })
+    const data = await response.json()
+    if(data.status === 'ok'){
+      setUname(data.name)
+    }
+    else{
+      alert(data.error)
+    }
+  }
+
+  useEffect(()=>{
+    const token = localStorage.getItem('token')
+    if(token){
+      displayProfile()
+    }
+  },[])
 
   return (
     <div className="Home">
       <SideBar />
       <div className="container">
        <h1 className="title">Home</h1>
-        
+        <div className="contentcontainer">
+          Welcome back, {uname}
+        </div>
       </div>
+      
     </div>
   );
 };
